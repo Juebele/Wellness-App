85583179c332ebd0abba7f27ef018393d11ed7ff
Tim Dolan <timmydolan14@gmail.com>
2023-05-15 14:44:27 -0400

    Created MEALPLAN.js DB

---
+++ models/mealplan.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Mealplan extends Model { }
+
+Mealplan.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        userid: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        breakfast: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        lunch: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        dinner: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        dayid: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Mealplan',
+    }
+);
+
+
+module.exports = Mealplan;
