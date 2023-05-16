7f01ce7c95bb9d3aca22983059f253ee7dfe272b
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-05-16 13:09:41 -0400

    Seed data working

---
+++ seeds/index.js
@@ -0,0 +1,14 @@
+const sequelize = require('../config/connection');
+const seedmealplan = require('./mealplanData');
+const seedUserData = require('./userData');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true});
+    await seedmealplan();
+    await seedUserData();
+    
+
+    process.exit(0);
+};
+
+seedAll();
