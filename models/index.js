29cff522c40161cb27aea290a3e110f91c7ce720
ddbriggs95 <dowlingbriggs@gmail.com>
2023-05-16 13:37:09 -0500

    Added associations

--- models/index.js
+++ models/index.js
@@ -2,7 +2,13 @@ const User = require('./User');
 const Mealplan = require('./mealplan')
 
 //associations
+User.hasMany(Mealplan, {
+    foreignKey: 'userid'
+})
 
+Mealplan.belongsTo(User, {
+    foreignKey: 'userid'
+})
 
 
 module.exports = { User, Mealplan };
