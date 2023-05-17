4c7084fa8a2dab1651b63df78a304c3368290f95
Tim Dolan <timmydolan14@gmail.com>
2023-05-17 12:02:18 -0400

    Updated MealPlan function and HB file

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,22 +3,22 @@ const {User, Mealplan} = require('../models/index.js');
 
 //localhost:3001/
 //get all Users
-router.get ('/', async (req, res) => {
-    try {
-        const dbUserData = await User.findAll({});
+// router.get ('/', async (req, res) => {
+//     try {
+//         const dbUserData = await User.findAll({});
 
-        const mappedData = dbUserData.map((user) => user.get({plain: true}))
-        console.log(mappedData);
-        // res.json(dbUserData);
+//         const mappedData = dbUserData.map((user) => user.get({plain: true}))
+//         console.log(mappedData);
+//         // res.json(dbUserData);
 
-        //res.render to populate the main
-        res.render('homepage', {users: mappedData});
+//         //res.render to populate the main
+//         res.render('homepage', {users: mappedData});
 
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
-});
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).json(err);
+//     }
+// });
 
 //localhost:3001/signup
 //request to create new user and put into db when user clicks sign up
@@ -38,7 +38,41 @@ router.post('/signup', async (req, res) => {
 
 });
 
-
+// localhost:3001/api/mealplan
+// Get Info
+router.get('/', async (req, res) => {
+    try {
+      const mpUserData = await Mealplan.findAll({});
+      const mpMappedData = mpUserData.map((mealplan) => mealplan.get({plain: true}))
+      console.log(mpMappedData);
+      // res.status(200).json(mpUserData);
+      res.render('homepage', {mealplan: mpMappedData});
+  
+  
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+  
+  // localhost:3001/api/mealplan
+  // Post Info
+  router.post('/', async (req, res) => {
+    try {
+      const newMPdata = await Mealplan.create({
+        userid: req.body.userid,
+        breakfast: req.body.breakfast,
+        lunch: req.body.lunch,
+        dinner: req.body.dinner,
+        dayid: req.body.dayid,
+      });
+      console.log(newMPdata)
+      res.status(200).json(newMPdata);
+    } catch (err) {
+      console.log(err)
+      res.status(400).json(err);
+    }
+  });
 
 
 module.exports = router;
