15803df077004ff38922e3a9445ba16d07a43696
Juebele <eronuebele@gmail.com>
2023-05-17 10:07:25 -0400

    made handlebars progress

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,13 +4,11 @@ const {User, Mealplan} = require('../models/index.js');
 router.get ('/', async (req, res) => {
     try {
         const dbUserData = await User.findAll({});
-        const mappedData = dbUserData.map((user) => {
-            user.get({plain: true})
-        })
-        console.log(dbUserData);
-        res.json(dbUserData);
+        const mappedData = dbUserData.map((user) => user.get({plain: true}))
+        console.log(mappedData);
+        // res.json(dbUserData);
         //res.render to populate the main
-        res.render('homepage', {});
+        res.render('homepage', {userid:mappedData[0].id});
 
     } catch (err) {
         console.log(err);
