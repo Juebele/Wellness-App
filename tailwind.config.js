c24ea58a4d28f11af4bb8dee712d4802ec630660
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-05-18 11:52:49 -0400

    setting up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/layouts/main.handlebars",
+  "./views/homepage.handlebars",
+  "./views/signup.handlebars"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
