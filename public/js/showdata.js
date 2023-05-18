b94765e4575f310e77b5b27a0a8fd3f5ef5c843c
ddbriggs95 <dowlingbriggs@gmail.com>
2023-05-18 14:50:55 -0500

    added container to house displayed db data of mealplans

---
+++ public/js/showdata.js
@@ -0,0 +1,31 @@
+
+async function getData() {
+    try {
+        const response = await fetch('/mealplans', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+
+        });
+
+        if(response.ok) {
+            const mealPlans = await response.json();
+
+            const container = document.getElementById('mealPlanContainer');
+
+            container.innerHTML= '';
+
+            mealPlans.forEach((mealPlan) => {
+                const mealPlanElement = document.createElement('div');
+                mealPlanElement.textContent = `Breakfast: ${mealPlan.breakfast}, Lunch: ${mealPlan.lunch}, Dinner: ${mealPlan.dinner}`;
+                container.appendChild(mealPlanElement);
+            })
+        } else (error) 
+            console.log('Failed to fetch data from server');
+        
+
+    } catch (error) {
+
+    }
+}
