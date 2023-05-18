3a9e8f129fe887a5396291083586771f1aa98507
ddbriggs95 <dowlingbriggs@gmail.com>
2023-05-17 20:16:52 -0500

    Added submit button logic to take all user inputted mealplans and send to db - just in not correct format due to i bealieve manually inputted dayid and userid

--- public/js/inputmealplan.js
+++ public/js/inputmealplan.js
@@ -2,33 +2,78 @@
 
 
 const submitMP = async (event) => {
-    var bfmeals = [];
-    const mealPlanBfast = document.getElementsByClassName('bfastText');
-    for(i = 0; i < mealPlanBfast.length; i++) {
-       let mealPlanBfast = mealPlanBfast[i].value;
+
+    //grab all elements from the page and assign to variable
+    const mealPlanBfastElements = document.getElementsByClassName('bfastText');
+    const mealPlanLunchElements = document.getElementsByClassName('lunchText');
+    const mealPlanDinnerElements = document.getElementsByClassName('dinnerText');
+
+    //get all inputted breakfast items
+    const breakfastItems = [];
+    for(i = 0; i < mealPlanBfastElements.length; i++) {
+       let mealPlanBfast = mealPlanBfastElements[i].value;
        let jsonStringBfast = JSON.stringify(mealPlanBfast);
-        console.log(jsonStringBfast);
-        // let mealPlanLunch = document.getElementById('lunchText').value;
-        // let mealPlanDinner = document.getElementById('dinnerText').value;
-        
+      //console.log(jsonStringBfast);
+      breakfastItems.push(jsonStringBfast);
+     
     }
-  
+    const breakfastString = breakfastItems.join(',');
 
-    //grab meals inputted into text areas
-   //const mealPlansArr = document.getElementsByClassName('meal').value;
+    //get all inputted lunch items
+    const lunchItems = [];
+    for(i = 0; i < mealPlanLunchElements.length; i++) {
+      let mealPlanLunch = mealPlanLunchElements[i].value;
+      let jsonStringLunch = JSON.stringify(mealPlanLunch);
+     //console.log(jsonStringLunch);
+     lunchItems.push(jsonStringLunch);
+    
+   }
+   const lunchString = lunchItems.join(',');
 
 
-   //turn those into json
-//    let jsonString = JSON.stringify(mealPlanBfast);
-//    let jsonString1 = JSON.stringify(mealPlanLunch);
-//    let jsonString2 = JSON.stringify(mealPlanDinner);
-   
+   //get all inputted dinner items
+   const dinnerItems = [];
+   for(i = 0; i < mealPlanDinnerElements.length; i++) {
+      let mealPlanDinner = mealPlanDinnerElements[i].value;
+      let jsonStringDinner = JSON.stringify(mealPlanDinner);
+     //console.log(jsonStringDinner);
+    dinnerItems.push(jsonStringDinner);
+   }
+   const dinnerString = dinnerItems.join(',');
 
-   console.log(jsonString);
-   console.log(mealPlanBfast);
-   console.log(mealPlanLunch);
-   console.log(mealPlanDinner);
-} 
+
+
+   //Combine the JSON Strings
+   const combinedJSON = JSON.stringify({
+      breakfast: breakfastString,
+      lunch: lunchString,
+      dinner: dinnerString,
+      dayid: 4,
+      userid: 4
+   });
+   console.log(combinedJSON);
+
+   //send the JSON data to the server
+   try {
+      const response = await fetch('/', {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json'
+         },
+         body: combinedJSON
+      });
+
+      if(response.ok) {
+         console.log('Data sent to the database');
+      } else {
+         console.log('Failed to send data to the database');
+      }
+
+   } catch (error) {
+      console.error('Error:', error)
+   }
+
+};
 
 
 submitMP();
