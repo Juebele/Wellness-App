9e212374f9aabc12e8c197eb82e03677d68ab58b
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-05-18 13:09:31 -0400

    Switching to main for pull

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/layouts/main.handlebars",
-  "./views/homepage.handlebars",
-  "./views/signup.handlebars"
+  content: ["./views/layouts/main.handlebars"
 ],
   theme: {
     extend: {},
